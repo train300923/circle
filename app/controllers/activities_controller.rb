5f2d1809bade8e0a34e357bbdfce26d7e8b3f74a
Guilhem Maffre-Bauge <gmaffre@hotmail.com>
2024-02-17 16:20:25 +0100

    installed geocoding

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,11 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = Activity.all
+    @activities = Activity.where((latitude-params).abs < 5, (longitude-params).abs < 5)
   end
+
+def category
+  @category = Activity.category.find(params[])
+end
 
   def show
     @activity = Activity.find(params[:id])
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -6,4 +6,6 @@ class Activity < ApplicationRecord
   validates :description, presence: true
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
