ebf1eebc71a2b6e895ec16c5b0fcbf9817cf1f80
alex bgx <a.bourgeaux@gmail.com>
2024-02-19 11:47:26 +0100

    map on it sway

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -9,5 +9,13 @@ class ActivitiesController < ApplicationController
 
   def show
     @activity = Activity.find(params[:id])
+    if @activity.geocoded?
+    @markers = [{
+      lat: @activity.latitude,
+      lng: @activity.longitude,
+      info_window_html: render_to_string(partial: "info_window", locals: {activity: @activity}),
+      marker_html: render_to_string(partial: "marker", locals: {activity: @activity})
+    }]
+    end
   end
 end
