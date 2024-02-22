1a8fc089e04929c3acd1df2f5d34ae6a16ec9ac2
Guilhem Maffre-Bauge <gmaffre@hotmail.com>
2024-02-22 22:05:20 +0100

    finished basic activity route

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,11 +1,31 @@
 class ActivitiesController < ApplicationController
+  before_action :set_query_param, only: [:index, :category, :info_activity]
+
   def index
-    @activities = Activity.near(params[:query], 10)
+    if params[:query]
+      @activities = Activity.near(params[:query], 10)
+    elsif params[:search]
+      search_params = params[:search]
+      @date = search_params[:date]
+      @time = search_params[:time]
+      @max_price = search_params[:maximum_price].to_f
+      @city = search_params[:city]
+      @category = search_params[:category]
+      @activities = Activity.near(@city, 10).where(category: @category).where("price <= ?", @max_price)
+    end
   end
 
-  def category
+  def category_list
     @activities = Activity.near(params[:query], 10)
-    @activities_category = Activity.where(category: params[:category])
+    @city = params[:query]
+    # @activities_category = @activities.where(category: params[:category])
+  end
+
+  def info_activity
+    # @date = params[:date]
+    # @time = params[:time]
+    # @max_price = params[:maximum_price]
+    # redirect_to category_activities_path(date: @date, time: @time, maximum_price: @max_price, query_param: @query_param, category: params[:category])
   end
 
   def show
@@ -18,5 +38,11 @@ class ActivitiesController < ApplicationController
         marker_html: render_to_string(partial: "marker")
     }]
     end
+  end
+
+  private
+
+  def set_query_param
+    @query_param = params[:query]
   end
 end
