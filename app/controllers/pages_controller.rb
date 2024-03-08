d672ad050e516cf1a6fda8e28affcf15f1aa9299
Guilhem Maffre-Bauge <gmaffre@hotmail.com>
2024-03-08 22:22:20 +0100

    fixed homepage memory image

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @random_activity = Activity.all.sample(1).sole
+    @random_activity = Activity.find_by(name: "Clubbing")
     @users_memory = User.all.sample(8)
   end
 
