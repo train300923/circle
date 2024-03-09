2f9f27dac6c6526b6806cdc7825ac083caa8d8b0
Kaouthar Elherch <kaouthar.elherch@gmail.com>
2024-03-09 12:37:59 +0100

    added error pages

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Circle
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
-
+    config.exceptions_app = self.routes
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
