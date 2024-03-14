2d20c8c66342e83b0f3c0795d1697040095029e5
alex bgx <a.bourgeaux@gmail.com>
2024-03-14 20:50:54 +0100

    new tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
 end
 
 # app/controllers/application_controller.rb
-
-
-def default_url_options
-  { host: ENV["DOMAIN"] || "localhost:3000" }
-end
